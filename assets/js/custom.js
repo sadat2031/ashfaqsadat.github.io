dcc149e5b2615923606873695efe37130b490fe6
Md. Ashfaq Sadat <sadat2031@gmail.com>
2020-07-26 13:13:29 +0600

    added CSS loader

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,23 @@
+
+
+// Loader 
+    $(document).ready(function() {
+    //Preloader
+    preloaderFadeOutTime = 500;
+    function hidePreloader() {
+    var preloader = $('.spinner-wrapper');
+    preloader.fadeOut(preloaderFadeOutTime);
+    }
+    hidePreloader();
+    });
+
+
+
 // Gallery Carousel
   $('.carousel').carousel({
     autoplay: true,
     dots: true,
     loop : true,
   })
+
+
